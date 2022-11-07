565c932c47f7201e8641e5cbc363f9331937f253
Zahari Kassabov <zk261@cam.ac.uk>
2022-11-07 18:51:46 +0000

    Factor out functionality to deal with bsm params
    Honestly I don't understand most of this. Just move it where it doesn't
    have to be looked at as much.
    
    Fix a couple bugs.
    
    Add a function to get a real name from a name with some accuracy info
    attached.

--- validphys2/src/validphys/config.py
+++ validphys2/src/validphys/config.py
@@ -52,6 +52,7 @@ from validphys.gridvalues import LUMI_CHANNELS
 from validphys.paramfits.config import ParamfitsConfig
 
 from validphys.plotoptions import get_info
+from validphys import bsmnames
 
 import validphys.scalevariations
 
@@ -516,61 +517,15 @@ class CoreConfig(configparser.Config):
         bsm_sector = dataset.get("bsm_sector")
         bsm_order = dataset.get("bsm_order")
 
-        if bsm_fac_data is not None:
-            if bsm_order is not None :
-                new_bsm_fac_data_names = []
-                # Now go through all operators, and see which are in the appropriate sector.
-                for op in bsm_fac_data_names:
-                    if op in bsm_sector_data[bsm_sector]:
-                        if bsm_order == "LO_QUAD":
-                            new_bsm_fac_data_names += ["LO_LIN_" + op]
-                        elif bsm_order == "NLO_QUAD":
-                            new_bsm_fac_data_names += ["NLO_LIN_" + op]
-                        else:
-                            new_bsm_fac_data_names += [bsm_order + "_" + op]
-                    else:
-                        new_bsm_fac_data_names += ["None_" + op]
+        bsm_data = bsmnames.get_bsm_data(
+            bsm_sector,
+            bsm_order,
+            bsm_fac_data,
+            bsm_sector_data,
+            bsm_fac_data_names,
+            n_bsm_fac_data,
+        )
 
-                # This takes care of linear. Now need to do the same for quadratic, if it's 
-                # switched on...
-
-                if bsm_order in ["LO_QUAD", "NLO_QUAD"]:
-                    # Some care needed here...
-                    new_bsm_fac_quad_names = pd.DataFrame(index=range(n_bsm_fac_data), columns=range(n_bsm_fac_data))
-
-                    for i in range(n_bsm_fac_data):
-                        for j in range(n_bsm_fac_data):
-                            if bsm_fac_data_names[i] in bsm_sector_data[bsm_sector]:
-                                if bsm_fac_data_names[j] in bsm_sector_data[bsm_sector]:
-                                    if i == j:
-                                         new_bsm_fac_quad_names.iloc[i,j] = bsm_order + "_" + bsm_fac_data_names[i]
-                                    else:
-                                         new_bsm_fac_quad_names.iloc[i,j] = bsm_order + "_" + bsm_fac_data_names[i] + "*" + bsm_fac_data_names[j]
-                                         new_bsm_fac_quad_names.iloc[j,i] = bsm_order + "_" + bsm_fac_data_names[j] + "*" + bsm_fac_data_names[i]
-                                else:
-                                    if i == j:
-                                         new_bsm_fac_quad_names.iloc[i,j] = "None_" + bsm_fac_data_names[i]
-                                    else:
-                                        # Ignore contribution entirely
-                                        new_bsm_fac_quad_names.iloc[i,j] = "None_" + bsm_fac_data_names[i] + "*" + bsm_fac_data_names[j]
-                                        new_bsm_fac_quad_names.iloc[j,i] = "None_" + bsm_fac_data_names[j] + "*" + bsm_fac_data_names[i]
-                            else:
-                                if i == j:
-                                    new_bsm_fac_quad_names.iloc[i,j] = "None_" + bsm_fac_data_names[i]
-                                else:
-                                    new_bsm_fac_quad_names.iloc[i,j] = "None_" + bsm_fac_data_names[i]+bsm_fac_data_names[j]
-                                    new_bsm_fac_quad_names.iloc[j,i] = "None_" + bsm_fac_data_names[i]+bsm_fac_data_names[i]
-                    new_bsm_fac_quad_names = new_bsm_fac_quad_names.values.tolist()         
-                else:
-                    new_bsm_fac_quad_names = None
-
-            else:
-                new_bsm_fac_data_names = None
-                new_bsm_fac_quad_names = None
-
-        else:
-            new_bsm_fac_data_names = None
-            new_bsm_fac_quad_names = None
 
         return DataSetInput(
             name=name,
@@ -579,8 +534,7 @@ class CoreConfig(configparser.Config):
             frac=frac,
             weight=weight,
             custom_group=custom_group,
-            bsm_fac_data_names=new_bsm_fac_data_names,
-            bsm_fac_quad_names=new_bsm_fac_quad_names,
+            **bsm_data
         )
 
     def parse_use_fitcommondata(self, do_use: bool):
