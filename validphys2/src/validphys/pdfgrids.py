90951ad75f5429e81f2e18d2000664d1fd6494b6
Manuel Morales <manuelmorales@dhcp-172-24-143-119.eduroam.wireless.private.cam.ac.uk>
2023-04-19 17:12:22 +0100

    name variables consistently

--- validphys2/src/validphys/pdfgrids.py
+++ validphys2/src/validphys/pdfgrids.py
@@ -234,8 +234,8 @@ def lumigrid1d(
         y_max =  np.log(1/sqrt_tau)
 
         # no lower boundary on the absolute value of the rapidity by default
-        y_min_in = 0
-        y_max_in = 0
+        y_min_low = 0
+        y_max_low = 0
 
         if y_cut is not None:
             if -y_cut > y_min and  y_cut < y_max:
@@ -244,13 +244,13 @@ def lumigrid1d(
 
             if y_cut_low is not None:
                 if -y_cut_low > y_min and  y_cut_low < y_max:
-                    y_min_in = -y_cut_low
-                    y_max_in = y_cut_low
+                    y_min_low = -y_cut_low
+                    y_max_low = y_cut_low
                 else:
                     raise ValueError(f"y_cut_low has to be smaller than y_cut.")
 
         print(f"y_min = {y_min}   y_max = {y_max}   ")
-        print(f"y_min_in = {y_min_in}   y_max_in = {y_max_in}   \n")
+        print(f"y_min_low = {y_min_low}   y_max_low = {y_max_low}   \n")
         for irep in range(nmembers):
             # Eq.(3) in arXiv:1607.01831
             f = lambda y: evaluate_luminosity(
@@ -258,7 +258,7 @@ def lumigrid1d(
                 sqrt_tau * np.exp(y), sqrt_tau * np.exp(-y),
                 lumi_channel
             )
-            res = integrate.quad(f, y_min, y_max, epsrel=5e-4, limit=50)[0] - integrate.quad(f, y_min_in, y_max_in, epsrel=5e-4, limit=50)[0]
+            res = integrate.quad(f, y_min, y_max, epsrel=5e-4, limit=50)[0] - integrate.quad(f, y_min_low, y_max_low, epsrel=5e-4, limit=50)[0]
 
             weights[irep, im] = res
 
