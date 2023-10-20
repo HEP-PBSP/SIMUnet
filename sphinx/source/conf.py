023e531908810fb212da53ea50d94c0bf0a7d65b
Elie Hammou <elie.hammouvictoroff@gmail.com>
2023-10-20 16:09:08 +0100

    Added PBSP logo

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -52,6 +52,8 @@ bibtex_reference_style = 'author_year'
 #html_theme = 'alabaster'
 html_theme = 'sphinx_book_theme'
 
+html_logo = "_static/PBSP_logo.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
