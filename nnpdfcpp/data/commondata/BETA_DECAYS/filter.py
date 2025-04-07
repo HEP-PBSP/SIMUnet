a5d70054e9f4e79da9fcddae2836caf89a58b749
Ella Cole <ellagcole02@gmail.com>
2025-04-07 15:57:18 +0100

    Beta decay data implementation

---
+++ nnpdfcpp/data/commondata/BETA_DECAYS/filter.py
@@ -0,0 +1,71 @@
+"""
+When running `python filter.py` the relevant data yaml
+file will be created in the `` directory.
+"""
+
+import yaml
+from filter_utils import get_data_values, get_uncertainties, get_kinematics
+
+# from nnpdf_data.filter_utils.utils import prettify_float
+
+# yaml.add_representer(float, prettify_float)
+
+
+def filter_BETA_DECAYS_data_central():
+    """
+    This function writes the central systematics to yaml files.
+    """
+
+    central_values = get_data_values()
+
+    kin = get_kinematics()
+
+    data_central_yaml = {"data_central": central_values}
+
+    kinematics_yaml = {"bins": kin}
+
+    # write central values and kinematics to yaml file
+    with open("data.yaml", "w") as file:
+        yaml.dump(data_central_yaml, file, sort_keys=False)
+
+    with open("kinematics.yaml", "w") as file:
+        yaml.dump(kinematics_yaml, file, sort_keys=False)
+
+def filter_BETA_DECAYS_get_systematics():
+    with open("metadata.yaml", "r") as file:
+        metadata = yaml.safe_load(file)
+
+    systematics = get_uncertainties()
+
+    error_definitions = {}
+    errors = []
+    counter_1 = 1
+    counter_2 = 0
+    for sys in systematics:
+        error_definitions[sys[0]['name']] = {
+                "description": "Uncorrelated statistical uncertainties",
+                "treatment": "ADD",
+                "type": "UNCORR",
+            }
+
+    for i in range(metadata['implemented_observables'][0]['ndata']):
+        error_value = {}
+
+        for sys in systematics:
+            # print(i)
+           
+            error_value[sys[0]['name']] = float(sys[0]['values'][i])
+
+        errors.append(error_value)
+
+    uncertainties_yaml = {"definitions": error_definitions, "bins": errors}
+
+    # write uncertainties
+    with open(f"uncertainties.yaml", 'w') as file:
+        yaml.dump(uncertainties_yaml, file, sort_keys=False)
+
+
+
+if __name__ == "__main__":
+    filter_BETA_DECAYS_data_central()
+    filter_BETA_DECAYS_get_systematics()
---
+++ nnpdfcpp/data/commondata/BETA_DECAYS/filter_utils.py
@@ -0,0 +1,83 @@
+"""
+This module contains helper functions that are used to extract the data values 
+from the rawdata files.
+"""
+
+import yaml
+import pandas as pd
+import numpy as np
+
+
+def get_data_values():
+    """
+    returns the central data values in the form of a list.
+    """
+
+    data_central = []
+
+    raw_data = f"rawdata/central.yaml"
+
+    with open(raw_data, 'r') as file:
+        input = yaml.safe_load(file)
+
+    values_string = input['central_vals']
+    values =  [float(val.strip()) for val in values_string.split(",")]
+
+    for value in values:
+        data_central.append(value)
+
+    return data_central
+
+
+def get_kinematics():
+    """
+    returns the kinematics in the form of a list of dictionaries.
+    """
+    kin = []
+
+    raw_data = f"rawdata/central.yaml"
+
+    with open(raw_data, 'r') as file:
+        input = yaml.safe_load(file)
+
+    values_string = input['central_vals']
+    values =  [float(val.strip()) for val in values_string.split(",")]
+    num_of_bins = len(values)
+
+    for i in range(num_of_bins):
+        kin_value = {
+            'abs_eta': {'min': None, 'mid': None, 'max': None},
+            'm_W2': {'min': None, 'mid': None, 'max': None},
+            'sqrts': {'min': None, 'mid': None, 'max': None},
+        }
+        kin.append(kin_value)
+
+    return kin
+
+
+def get_uncertainties():
+    """
+    returns the uncertainties.
+    """
+    covmat_path = 'rawdata/covmat.yaml'
+
+    uncertainties = []
+
+    data_central = get_data_values()
+
+    with open(covmat_path, 'r') as file:
+        data = yaml.safe_load(file)
+
+    stats = []
+    for row in data['experimental_cov']:
+        for value in row:
+            if value !=0:
+                unc = round(np.sqrt(value),10)
+                stats.append(unc)
+   
+    uncertainties.append([{"name": 'stat', "values": stats}])
+
+    return uncertainties
+
+if __name__ == "__main__":
+    get_kinematics()
