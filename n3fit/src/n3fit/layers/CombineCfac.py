ad83f99928c3e612ef3d53653a06591a00e6aa2d
Francesco Merlotti <fm627@login-q-2.data.cluster>
2025-05-15 13:28:53 +0100

    update of combine cfac keras-> tf.keras

--- n3fit/src/n3fit/layers/CombineCfac.py
+++ n3fit/src/n3fit/layers/CombineCfac.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
-from keras.constraints import NonNeg
+from tensorflow.keras.constraints import NonNeg
 
 import numpy as np
 import hashlib
